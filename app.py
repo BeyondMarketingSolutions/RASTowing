2ae9fe8c0cd64be9ebb2c9267aaa89297d453be5
mauran <solutionsbeyondmarketing@gmail.com>
2022-05-02 19:48:32 +0200

    Added two rows in DriversDB

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-
 from flask import Flask, render_template, request
 from helper.InternalHelper import InternalHelper
 from helper.ExcelHelper import ExcelHelper
@@ -7,6 +6,7 @@ from helper.Services import Services
 app = Flask(__name__)
 helper = InternalHelper()
 
+
 @app.route('/')
 def towing_dashboard():
     return render_template('main.html')
@@ -14,19 +14,19 @@ def towing_dashboard():
 
 @app.route('/', methods=['POST'])
 def render_results():
-
     client_location, preferred_client_destination, service_filter, vehicle_type = InternalHelper.input_data_elaborate(
         request)
     drivers_basic_data = ExcelHelper.retrieve_drivers_data()
-    service_value = Services[service_filter].value;
+    service_value = Services[service_filter].value
     drivers_basic_data = [driver_data for driver_data in drivers_basic_data if driver_data[service_value] == 'Yes']
     if len(drivers_basic_data) == 0:
         return render_template('main.html', drivers=None, messages=['No driver provides the selected service!'])
     else:
         driversLocations = [data['Address'] for data in drivers_basic_data]
         driversResponse = helper.retrieve_nearest_drivers(driversLocations, client_location, drivers_basic_data)
-        estimatedPriceResponse = helper.calculate_estimated_price_based_on_service(client_location, preferred_client_destination,
-                                                                      service_filter, vehicle_type)
+        estimatedPriceResponse = helper.calculate_estimated_price_based_on_service(client_location,
+                                                                                   preferred_client_destination,
+                                                                                   service_filter, vehicle_type)
         return render_template('main.html', drivers=helper.normalize_tel_values(driversResponse),
                                estimatedPrice=estimatedPriceResponse,
                                messages=None)
